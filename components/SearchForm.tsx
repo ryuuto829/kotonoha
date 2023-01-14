9ab888977266d36ed85913210efc5795a5a9c499
ryuuto829 <ryuuto829@gmail.com>
2023-01-14 16:00:27 +0200

    feat: add search form

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -1,23 +1,51 @@
-import { useQuery } from '@tanstack/react-query'
+import { useMutation } from '@tanstack/react-query'
+import { useRouter } from 'next/router'
+import { useEffect, FormEvent } from 'react'
 
 export default function SearchForm() {
-  const { isLoading, isError, data, error } = useQuery({
-    queryKey: ['myQuery'],
-    queryFn: async () => {
-      const data = await fetch('/api/search/words?keyword=funsui')
+  const router = useRouter()
+  const searchWord = router.query.q
+
+  const searchResult = useMutation({
+    mutationFn: async (keyword: string) => {
+      if (!keyword) return null
+
+      const data = await fetch(`/api/search/words?keyword=${keyword}`)
       const json = await data.json()
-      console.log(json)
+
+      console.log(json.data)
+
       return json
     }
   })
 
+  useEffect(() => {
+    if (searchWord) {
+      searchResult.mutate(searchWord)
+    }
+  }, [searchWord])
+
+  const handleSearchFormSubmit = (event: FormEvent) => {
+    event.preventDefault()
+
+    const formData = new FormData(event.target as HTMLFormElement)
+    const searchInput = formData.get('search')
+
+    router.push(`/search?q=${searchInput}`)
+  }
+
   return (
     <>
-      <input type="text" placeholder="Search a word.." />
-      {`data: ${data}`}
-      {data &&
-        data.data.map((query: any) => {
-          return <div key={query.slug}>{query.slug}</div>
+      <form onSubmit={handleSearchFormSubmit}>
+        <input type="text" placeholder="Search a word.." name="search" />
+        <button type="submit">Search</button>
+      </form>
+
+      {searchResult.isLoading && <div>Loading ...</div>}
+
+      {searchResult.data &&
+        searchResult.data.data.map((result) => {
+          return <div key={result.slug}>{result.slug}</div>
         })}
     </>
   )
