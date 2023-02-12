c91b861b5ff4f84957bd10c257d77de14e27dfe0
ryuuto829 <ryuuto829@gmail.com>
2023-02-12 13:32:44 +0200

    refactor: update styles for vocabulary page

--- pages/vocabulary.tsx
+++ pages/vocabulary.tsx
@@ -1,12 +1,20 @@
+import { useState, useEffect, ReactNode } from 'react'
 import { useRxCollection } from 'rxdb-hooks'
-import { useState, useEffect } from 'react'
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
 import * as Switch from '@radix-ui/react-switch'
-
+import {
+  DotsHorizontalIcon,
+  TrashIcon,
+  Pencil2Icon,
+  PlusIcon,
+  ArchiveIcon
+} from '@radix-ui/react-icons'
 import CalendarDaysIcon from '@heroicons/react/24/outline/CalendarDaysIcon'
 import HashtagIcon from '@heroicons/react/24/outline/HashtagIcon'
+import ArrowsUpDownIcon from '@heroicons/react/24/outline/ArrowsUpDownIcon'
 
 import AddWordDialog from '../components/AddWordDialog'
+import { REVIEW_STATUS } from '../components/AddWordDialog'
 
 const SORTING_OPTIONS = [
   {
@@ -22,14 +30,275 @@ const SORTING_OPTIONS = [
 ]
 const WORDS_LIMIT_OPTIONS = ['25', '50', '75', '100', '150', '200']
 
+function MenuTriggerButton({
+  label,
+  ariaLabel,
+  icon
+}: {
+  label?: string
+  ariaLabel?: string
+  icon?: ReactNode
+}) {
+  return (
+    <DropdownMenu.Trigger
+      aria-label={ariaLabel}
+      className={`
+      inline-flex items-center space-x-2
+      p-1.5 text-base h-7 rounded-[3px]
+      hover:bg-white hover:bg-opacity-5
+      data-[state=open]:bg-white data-[state=open]:bg-opacity-5
+      `}
+    >
+      {icon}
+      {label && <span>{label}</span>}
+    </DropdownMenu.Trigger>
+  )
+}
+
+function MenuRadioItem({
+  value,
+  icon,
+  label,
+  checkedValue
+}: {
+  value: string
+  icon?: ReactNode
+  label: string
+  checkedValue: string
+}) {
+  return (
+    <DropdownMenu.RadioItem
+      value={value}
+      className={`
+      flex items-center
+      mx-1 rounded-[3px] h-7
+      hover:bg-white hover:bg-opacity-10 cursor-pointer
+      ${checkedValue === value ? 'text-blue-300  ' : ''}
+      `}
+    >
+      <div className="ml-2.5 mr-1 flex items-center justify-center">{icon}</div>
+      <div className="ml-1.5 mr-3 flex-1">{label}</div>
+    </DropdownMenu.RadioItem>
+  )
+}
+
+function SortingMenu({
+  isAscending,
+  sortOption,
+  changeAscending,
+  changeSortOption
+}: {
+  isAscending: boolean
+  sortOption: string
+  changeAscending: (value: boolean) => void
+  changeSortOption: (value: string) => void
+}) {
+  return (
+    <DropdownMenu.Root modal={false}>
+      <MenuTriggerButton
+        label="Sort"
+        ariaLabel="Sorting options"
+        icon={<ArrowsUpDownIcon className="w-4 h-4" />}
+      />
+      <DropdownMenu.Portal>
+        <DropdownMenu.Content
+          align="start"
+          sideOffset={5}
+          className={`
+          w-[230px] py-2 rounded bg-[rgb(37,37,37)]
+          text-sm text-white text-opacity-80
+          `}
+        >
+          <form
+            className={`
+            flex items-center justify-between mx-1
+            rounded-[3px] h-7 hover:bg-white hover:bg-opacity-10 cursor-pointer
+            `}
+          >
+            <label
+              htmlFor="ascending-mode"
+              className="ml-2.5 mr-1 flex-1 cursor-pointer"
+            >
+              Ascending
+            </label>
+            <Switch.Root
+              id="ascending-mode"
+              checked={isAscending}
+              onCheckedChange={changeAscending}
+              className={`
+              box-content relative
+              ml-1.5 mr-3 p-0.5  w-[26px] h-[14px] rounded-full shadow-sm
+              bg-[rgba(202,204,206,0.3)] data-[state=checked]:bg-[rgb(35,131,226)]
+              `}
+            >
+              <Switch.Thumb
+                className={`
+                block w-3.5 h-3.5 bg-white rounded-full shadow-sm
+                translate-x-0 data-[state=checked]:translate-x-[12px]
+                `}
+              />
+            </Switch.Root>
+          </form>
+
+          <DropdownMenu.Separator className="my-2 h-[1px] bg-white bg-opacity-20" />
+
+          <DropdownMenu.RadioGroup
+            value={sortOption}
+            onValueChange={changeSortOption}
+          >
+            {SORTING_OPTIONS.map((option) => (
+              <MenuRadioItem
+                key={option.value}
+                value={option.value}
+                icon={option.icon}
+                label={option.label}
+                checkedValue={sortOption}
+              />
+            ))}
+          </DropdownMenu.RadioGroup>
+        </DropdownMenu.Content>
+      </DropdownMenu.Portal>
+    </DropdownMenu.Root>
+  )
+}
+
+function ShowWordsMenu({
+  wordsNumber,
+  changeOffset,
+  changeWordsNumber
+}: {
+  wordsNumber: string
+  changeOffset: (value: number) => void
+  changeWordsNumber: (value: string) => void
+}) {
+  return (
+    <DropdownMenu.Root modal={false}>
+      <MenuTriggerButton
+        label={`Show: ${wordsNumber}`}
+        ariaLabel="Show words"
+      />
+
+      <DropdownMenu.Portal>
+        <DropdownMenu.Content
+          align="start"
+          sideOffset={5}
+          className={`
+          w-[230px] py-2 rounded bg-[rgb(37,37,37)]
+          text-sm text-white text-opacity-80
+          `}
+        >
+          <DropdownMenu.RadioGroup
+            value={wordsNumber}
+            onValueChange={(value) => {
+              changeOffset(0)
+              changeWordsNumber(value)
+            }}
+          >
+            {WORDS_LIMIT_OPTIONS.map((option) => (
+              <MenuRadioItem
+                key={option}
+                value={option}
+                label={option}
+                checkedValue={wordsNumber}
+              />
+            ))}
+          </DropdownMenu.RadioGroup>
+        </DropdownMenu.Content>
+      </DropdownMenu.Portal>
+    </DropdownMenu.Root>
+  )
+}
+
+function MoreOptionsMenu({ deleteWord, doc }) {
+  return (
+    <DropdownMenu.Root modal={false}>
+      <MenuTriggerButton
+        ariaLabel="More options"
+        icon={<DotsHorizontalIcon className="w-5 h-5" />}
+      />
+      <DropdownMenu.Portal>
+        <DropdownMenu.Content
+          align="end"
+          sideOffset={5}
+          className={`
+          w-[230px] py-2 rounded bg-[rgb(37,37,37)]
+          text-sm text-white text-opacity-80
+          `}
+        >
+          <AddWordDialog id={doc.id}>
+            <div className="mx-1 rounded-[3px] h-7 flex items-center hover:bg-white hover:bg-opacity-10 cursor-pointer">
+              <div className="ml-2.5 mr-1 flex items-center justify-center">
+                <Pencil2Icon className="w-4 h-4" />
+              </div>
+              <div className="ml-1.5 mr-3">Edit</div>
+            </div>
+          </AddWordDialog>
+          <div
+            className={`
+            flex items-center
+            mx-1 rounded-[3px] h-7
+           hover:bg-white hover:bg-opacity-10 cursor-pointer
+            `}
+            onClick={() => deleteWord(doc.id)}
+          >
+            <div className="ml-2.5 mr-1 flex items-center justify-center">
+              <TrashIcon className="w-4 h-4" />
+            </div>
+            <div className="ml-1.5 mr-3">Delete</div>
+          </div>
+        </DropdownMenu.Content>
+      </DropdownMenu.Portal>
+    </DropdownMenu.Root>
+  )
+}
+
+function VocabularyList({ wordDocuments, deleteWord }) {
+  return (
+    <div className="flex flex-col space-y-2 border-y border-white border-opacity-20 divide-y divide-white divide-opacity-20">
+      <div className="grid grid-cols-[1fr_1fr_100px_50px] gap-2 px-2">
+        <div>Word</div>
+        <div>Meaning</div>
+        <div>Status</div>
+      </div>
+      {wordDocuments &&
+        wordDocuments.map((doc) => (
+          <div
+            key={doc.id}
+            className="grid grid-cols-[1fr_1fr_100px_50px] gap-2 px-2 py-4"
+          >
+            <div className="text-lg">{doc.word}</div>
+            <div className="text-sm">{doc.meaning}</div>
+            <div>
+              {REVIEW_STATUS.filter(
+                (el) => el.status === doc.reviewStatus?.toString()
+              ).map((el) => (
+                <div
+                  key={el.label}
+                  className="inline-flex items-center space-x-2"
+                >
+                  {el.icon}
+                  <span className="text-xs">{el.label}</span>
+                </div>
+              ))}
+            </div>
+            <div>
+              <MoreOptionsMenu deleteWord={deleteWord} doc={doc} />
+            </div>
+          </div>
+        ))}
+    </div>
+  )
+}
+
 export default function Vocabulary() {
   const wordsCollection = useRxCollection('words')
-  const [wordList, setWordList] = useState([])
+
+  const [wordDocuments, setWordDocuments] = useState()
+  const [showWords, setShowWords] = useState('25')
   const [sortOption, setSortOption] = useState('createdAt')
   const [isAscending, setIsAscending] = useState(false)
-  const [showWordsNumber, setShowWordsNumber] = useState('25')
-  const [loadMore, setLoadMore] = useState(0)
-  const [docsCount, setDocsCount] = useState(0)
+  const [wordDocumentsOffset, setWordDocumentsOffset] = useState(0)
+  const [wordDocumentsCount, setWordDocumentsCount] = useState(0)
 
   useEffect(() => {
     let querySub: any
@@ -38,25 +307,19 @@ export default function Vocabulary() {
       const query = wordsCollection
         .find()
         .sort({ [sortOption]: isAscending ? 1 : -1 })
-        .skip(loadMore)
-        .limit(Number(showWordsNumber))
+        .skip(wordDocumentsOffset)
+        .limit(Number(showWords))
 
       querySub = query.$.subscribe((results) => {
         console.log(results)
-        setWordList(results)
+
+        wordsCollection.count().exec().then(setWordDocumentsCount)
+        setWordDocuments(results)
       })
     }
 
-    return () => {
-      querySub?.unsubscribe()
-    }
-  }, [wordsCollection, sortOption, isAscending, showWordsNumber, loadMore])
-
-  useEffect(() => {
-    if (wordsCollection) {
-      wordsCollection.count().exec().then(setDocsCount)
-    }
-  }, [wordsCollection])
+    return () => querySub?.unsubscribe()
+  }, [wordsCollection, sortOption, isAscending, showWords, wordDocumentsOffset])
 
   const deleteWord = async (id) => {
     const wordDocument = wordsCollection?.findOne(id)
@@ -68,165 +331,78 @@ export default function Vocabulary() {
   return (
     <>
       {/* Tabs, add & review */}
-      <div>
-        <AddWordDialog>
-          <button>Add word</button>
-        </AddWordDialog>{' '}
-        <button>Review</button>
+      <div className="border-b border-white border-opacity-20 flex items-center justify-between">
+        <div className="flex items-center">
+          <div className="py-3 relative">
+            <div className="inline-flex items-center h-7 rounded px-2">All</div>
+            <div className="absolute border-b-2 border-white border-opacity-80 px-2 left-2 right-2 bottom-0"></div>
+          </div>
+          <div className="py-3 relative">
+            <div className="inline-flex items-center h-7 rounded px-2">
+              Due for SRS
+            </div>
+            <div className="absolute border-b-2 border-white border-opacity-80 px-2 left-2 right-2 bottom-0"></div>
+          </div>
+        </div>
+
+        <div className="flex items-center space-x-2">
+          <AddWordDialog>
+            <button className="flex items-center text-white border border-white border-opacity-20 px-3 rounded-[3px] h-8 space-x-2">
+              <PlusIcon className="w-4 h-4" />
+              <span>Add word</span>
+            </button>
+          </AddWordDialog>
+          <button className="flex items-center text-white bg-[rgb(35,131,226)] px-3 rounded-[3px] h-8 space-x-2">
+            <ArchiveIcon className="w-4 h-4" />
+            <span>Review</span>
+          </button>
+        </div>
       </div>
 
       {/* Sort, filter & search */}
-      <DropdownMenu.Root>
-        <DropdownMenu.Trigger asChild>
-          <button className="IconButton" aria-label="Customise options">
-            Sort
-          </button>
-        </DropdownMenu.Trigger>
-
-        <DropdownMenu.Portal>
-          <DropdownMenu.Content
-            className="w-[230px] bg-[rgb(37,37,37)] py-2 text-sm text-white text-opacity-80 rounded"
-            align="start"
-            sideOffset={5}
-          >
-            <form className="mx-1 rounded-[3px] h-7 flex items-center justify-between hover:bg-white hover:bg-opacity-10 cursor-pointer">
-              <label
-                className="ml-2.5 mr-1 flex-1 cursor-pointer"
-                htmlFor="ascending-mode"
-              >
-                Ascending
-              </label>
-              <Switch.Root
-                id="ascending-mode"
-                checked={isAscending}
-                onCheckedChange={setIsAscending}
-                className="ml-1.5 mr-3 p-0.5 box-content w-[26px] h-[14px] bg-[rgba(202,204,206,0.3)] rounded-full relative shadow-sm data-[state=checked]:bg-[rgb(35,131,226)]"
-              >
-                <Switch.Thumb className="block w-3.5 h-3.5 bg-white rounded-full shadow-sm translate-x-0 data-[state=checked]:translate-x-[12px]" />
-              </Switch.Root>
-            </form>
-
-            <DropdownMenu.Separator className="my-2 h-[1px] bg-white bg-opacity-20" />
-
-            <DropdownMenu.RadioGroup
-              value={sortOption}
-              onValueChange={setSortOption}
-            >
-              {SORTING_OPTIONS.map((option) => {
-                return (
-                  <DropdownMenu.RadioItem
-                    key={option.value}
-                    className={`mx-1 rounded-[3px] h-7 flex items-center hover:bg-white hover:bg-opacity-10 cursor-pointer ${
-                      sortOption === option.value ? 'text-blue-300  ' : ''
-                    }`}
-                    value={option.value}
-                  >
-                    <div className="ml-2.5 mr-1 flex items-center justify-center">
-                      {option.icon}
-                    </div>
-                    <div className="ml-1.5 mr-3 flex-1">{option.label}</div>
-                  </DropdownMenu.RadioItem>
-                )
-              })}
-            </DropdownMenu.RadioGroup>
-
-            <DropdownMenu.Arrow className="DropdownMenuArrow" />
-          </DropdownMenu.Content>
-        </DropdownMenu.Portal>
-      </DropdownMenu.Root>
-
-      <DropdownMenu.Root>
-        <DropdownMenu.Trigger asChild>
-          <button className="IconButton" aria-label="Customise options">
-            {`Show: ${showWordsNumber}`}
-          </button>
-        </DropdownMenu.Trigger>
-
-        <DropdownMenu.Portal>
-          <DropdownMenu.Content
-            className="w-[230px] bg-[rgb(37,37,37)] py-2 text-sm text-white text-opacity-80 rounded"
-            align="start"
-            sideOffset={5}
-          >
-            <DropdownMenu.RadioGroup
-              value={showWordsNumber}
-              onValueChange={(value) => {
-                setLoadMore(0)
-                setShowWordsNumber(value)
-              }}
-            >
-              {WORDS_LIMIT_OPTIONS.map((option) => {
-                return (
-                  <DropdownMenu.RadioItem
-                    key={option}
-                    className={`mx-1 rounded-[3px] h-7 flex items-center hover:bg-white hover:bg-opacity-10 cursor-pointer ${
-                      showWordsNumber === option ? 'text-blue-300  ' : ''
-                    }`}
-                    value={option}
-                  >
-                    <div className="ml-1.5 mr-3 flex-1">{option}</div>
-                  </DropdownMenu.RadioItem>
-                )
-              })}
-            </DropdownMenu.RadioGroup>
-
-            <DropdownMenu.Arrow className="DropdownMenuArrow" />
-          </DropdownMenu.Content>
-        </DropdownMenu.Portal>
-      </DropdownMenu.Root>
-
-      {/* Words */}
-      <div className="flex flex-col space-y-2">
-        <div className="grid grid-cols-3 gap-2 border border-gray-300">
-          <div>Word</div>
-          <div>Meaning</div>
-          <div>Status</div>
-        </div>
-        {wordList.length > 0 &&
-          wordList.map((doc, index) => (
-            <div
-              key={index}
-              className="grid grid-cols-3 gap-2 border border-gray-300 px-2 py-4"
-            >
-              <div className="text-lg">{doc.word}</div>
-              <div className="text-sm">{doc.meaning}</div>
-              <div>
-                <button onClick={() => deleteWord(doc.id)}>Delete</button>{' '}
-                <AddWordDialog id={doc.id}>
-                  <button>Edit</button>
-                </AddWordDialog>
-              </div>
-            </div>
-          ))}
+      <div className="flex items-center space-x-2 py-2">
+        <SortingMenu
+          isAscending={isAscending}
+          changeAscending={setIsAscending}
+          sortOption={sortOption}
+          changeSortOption={setSortOption}
+        />
+        <ShowWordsMenu
+          wordsNumber={showWords}
+          changeOffset={setWordDocumentsOffset}
+          changeWordsNumber={setShowWords}
+        />
       </div>
 
-      {/* Load More */}
-      <button
-        onClick={() => {
-          const count = loadMore - Number(showWordsNumber)
+      <VocabularyList wordDocuments={wordDocuments} deleteWord={deleteWord} />
 
-          if (count >= 0) {
-            setLoadMore(count)
-          }
-        }}
-      >
-        Prev page
-      </button>
-      <button
-        onClick={() => {
-          const count = loadMore + Number(showWordsNumber)
+      <div>
+        <button
+          onClick={() => {
+            const count = wordDocumentsOffset - Number(showWords)
 
-          if (count <= docsCount) {
-            setLoadMore(count)
-          }
-        }}
-      >
-        Next page
-      </button>
+            if (count >= 0) {
+              setWordDocumentsOffset(count)
+            }
+          }}
+        >
+          Prev page
+        </button>
+        <button
+          onClick={() => {
+            const count = wordDocumentsOffset + Number(showWords)
 
-      <div>{`Showing ${loadMore + 1}-${
-        loadMore + wordList.length
-      } of ${docsCount} Entries`}</div>
+            if (count <= wordDocumentsCount) {
+              setWordDocumentsOffset(count)
+            }
+          }}
+        >
+          Next page
+        </button>
+        <div>{`Showing ${wordDocumentsOffset + 1}-${
+          wordDocumentsOffset + (wordDocuments?.length || 0)
+        } of ${wordDocumentsCount} Entries`}</div>
+      </div>
     </>
   )
 }
